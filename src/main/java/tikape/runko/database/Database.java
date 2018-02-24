59cc6497658cfd90fa2385cfb48b49a96e6cf31f
nakkekakke <juntusen.niko@gmail.com>
2018-02-24 14:46:25 +0200

    cleanup

--- src/main/java/tikape/runko/database/Database.java
+++ src/main/java/tikape/runko/database/Database.java
@@ -15,33 +15,4 @@ public class Database {
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(databaseAddress);
     }
-
-    public void init() {
-        List<String> lauseet = sqliteLauseet();
-
-        // "try with resources" sulkee resurssin automaattisesti lopuksi
-        try (Connection conn = getConnection()) {
-            Statement st = conn.createStatement();
-
-            for (String lause : lauseet) {
-                System.out.println("Running command >> " + lause);
-                st.executeUpdate(lause);
-            }
-
-        } catch (Throwable t) {
-            // jos tietokantataulu on jo olemassa, ei komentoja suoriteta
-            System.out.println("Error >> " + t.getMessage());
-        }
-    }
-
-    private List<String> sqliteLauseet() {
-        ArrayList<String> lista = new ArrayList<>();
-
-        // tietokantataulujen luomiseen tarvittavat komennot suoritusjärjestyksessä : EI VALMIS
-        lista.add("CREATE TABLE Title (id integer PRIMARY KEY, person_id integer, "
-                + "nimi varchar(255), julkaisuvuosi integer, pituus integer, "
-                + "FOREIGN KEY (person_id) REFERENCES PersonTitle(id);");
-
-        return lista;
-    }
-}
+}    
