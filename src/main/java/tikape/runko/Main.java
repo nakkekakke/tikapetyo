b66c30c8965da3ed134c7443417aca627b21e6fb
Veikka <32365239+SirVeggie@users.noreply.github.com>
2018-02-24 17:05:26 +0200

    random elokuva

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -1,11 +1,13 @@
 package tikape.runko;
 
 import java.util.HashMap;
+import java.util.List;
 import spark.ModelAndView;
 import static spark.Spark.*;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 import tikape.runko.database.Database;
 import tikape.runko.database.TitleDao;
+import tikape.runko.domain.Title;
 
 public class Main {
 
@@ -18,20 +20,35 @@ public class Main {
             HashMap map = new HashMap<>();
             map.put("testi", "Tervehdys, cunts");
             map.put("feck", "THIS IS a kitten");
+            
+            /*
             String kohde;
             int rnd = (int)(Math.random()*2);
             if (rnd == 0) {
-                kohde = "/testi";
+                kohde = "elokuva1";
             } else {
-                kohde = "/titles";
+                kohde = "elokuva2";
             }
-            //System.out.println(kohde);
+            
             map.put("kohde", kohde);
-
+            */
+            
+            List<Title> titles = titleDao.findAll();
+            if (titles.size() == 0) {
+                map.put("kohdeNimi", "Movie list empty.");
+            } else {
+                int rnd = (int)(Math.random()*titles.size())+1;
+                String kohde = "/titles/" + rnd;
+                map.put("kohde", kohde);
+                String kohdeNimi = titleDao.findOne(rnd).getNimi();
+                map.put("kohdeNimi", kohdeNimi);
+            }
+            
+            
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
 
-        get("/testi", (req, res) -> {
+        get("/movie", (req, res) -> {
             HashMap map = new HashMap<>();
             map.put("titles", titleDao.findAll());
 
@@ -47,9 +64,11 @@ public class Main {
 
         get("/titles/:id", (req, res) -> {
             HashMap map = new HashMap<>();
-            map.put("titles", titleDao.findOne(Integer.parseInt(req.params("id"))));
+            Title title = titleDao.findOne(Integer.parseInt(req.params("id")));
+            map.put("name", title.getNimi());
+            map.put("title", title);
 
-            return new ModelAndView(map, "titles");
+            return new ModelAndView(map, "title");
         }, new ThymeleafTemplateEngine());
     }
 }
