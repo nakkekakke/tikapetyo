1910fed24d627d334cac65818e26e3328c41d3f0
porrasm <31691452+porrasm@users.noreply.github.com>
2018-02-25 03:01:56 +0200

    asd

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -18,6 +18,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Database database = new Database("jdbc:sqlite:imbd.db");
+        //database.resetDatabase();
         
         IMDBReader imdb = new IMDBReader(database);
         
